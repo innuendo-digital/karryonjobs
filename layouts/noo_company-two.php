7dcd2ab24f00d41443cd9943ccc916dbf05f420f
Gabriel Grieve <gabe@innuendodigital.com.au>
2018-03-01 11:15:46 +1100

    fixed mobile styling for company page

--- layouts/noo_company-two.php
+++ layouts/noo_company-two.php
@@ -20,9 +20,9 @@
                 </div>
             </div>
         </div>
+        <div class="clear"></div>
         <div class="<?php noo_main_class(); ?>">
             <div class="noo-company-content">
-                <div class="noo-company-heading"><?php _e('About the Company', 'noo'); ?></div>
                 <?php
                 $content = get_post_field( 'post_content', get_the_ID() );
                 $content = apply_filters('the_content', $content);
